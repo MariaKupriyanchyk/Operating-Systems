815f874c232fa94899bd25bfef334e720b88a8bc
Maria <gufi7de@gmail.com>
2025-10-02 20:42:09 +0300

    add realisation for new functions employee.cpp

--- employee.cpp
+++ employee.cpp
@@ -1,16 +1,54 @@
 #include "employee.h"
 #include <iomanip>
 #include <iostream>
-
-void printEmployee(const employee& e, double payRate) {
-	double salary = calculateSalary(e, payRate);
-
-	std::cout << "ID: " << e.num << "\t";
-	std::cout << "Name: " << e.name << "\t";
-	std::cout << "Hours: " << e.hours << "\t";
-	std::cout << "Salary: " << salary << "\n";
-}
+#include <regex>
 
 double calculateSalary(const employee& e, double payRate) {
 	return e.hours * payRate;
+}
+
+int getValidId() {
+    std::string input;
+    std::regex rgx("^[0-9]+$");
+    while (true) {
+        std::cout << "ID: ";
+        std::cin >> input;
+        if (std::regex_match(input, rgx)) {
+            return std::stoi(input);
+        }
+        else {
+            std::cout << "Invalid ID. Only digits allowed.\n";
+        }
+    }
+}
+
+void getValidName(char* name) {
+    std::string input;
+    std::regex pattern("^[A-Za-z]{1,9}$");
+    while (true) {
+        std::cout << "Name: ";
+        std::cin >> input;
+        if (std::regex_match(input, pattern)) {
+            std::strncpy(name, input.c_str(), 9);
+            name[9] = '\0';
+            return;
+        }
+        else {
+            std::cout << "Invalid name. Only letters (max 9).\n";
+        }
+    }
+}
+
+double getValidHours() {
+    std::string input;
+    std::regex rgx("^[0-9]+(\\.[0-9]+)?$");
+    while (true) {
+        std::cout << "Hours: ";
+        std::cin >> input;
+        if (std::regex_match(input, rgx)) {
+            double val = std::stod(input);
+            if (val >= 0) return val;
+        }
+        std::cout << "Invalid hours. Must be non-negative number.\n";
+    }
 }
