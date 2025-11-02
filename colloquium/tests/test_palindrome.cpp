acb10a6246dee89ec88fde0eeec36f91af81fff4
Maria <gufi7de@gmail.com>
2025-11-02 15:55:18 +0300

    add new tests

--- colloquium/tests/test_palindrome.cpp
+++ colloquium/tests/test_palindrome.cpp
@@ -1,9 +1,18 @@
 #include "palindrome.h"
 #include <gtest/gtest.h>
 
-TEST(PalindromeTests, BasicCases) {
-    EXPECT_TRUE(is_palindrome_number(121));
-    EXPECT_TRUE(is_palindrome_number(12321));
-    EXPECT_FALSE(is_palindrome_number(123));
-    EXPECT_FALSE(is_palindrome_number(-121));
+TEST(PalindromeTest, ThrowsOnEmptyInput) {
+    EXPECT_THROW(is_palindrome_number(""), std::invalid_argument);
+}
+
+TEST(PalindromeTest, ThrowsOnNonDigitInput) {
+    EXPECT_THROW(is_palindrome_number("12a21"), std::invalid_argument);
+    EXPECT_THROW(is_palindrome_number("-121"), std::invalid_argument);
+    EXPECT_THROW(is_palindrome_number("jdhfkd"), std::invalid_argument);
+    EXPECT_THROW(is_palindrome_number("abcba"), std::invalid_argument);
+}
+
+TEST(PalindromeTest, DetectsPalindromeCorrectly) {
+    EXPECT_TRUE(is_palindrome_number("12321"));
+    EXPECT_FALSE(is_palindrome_number("12345"));
 }
