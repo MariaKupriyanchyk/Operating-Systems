7123808ba6f114f7548effc43302cf89a2afb21d
Maria <gufi7de@gmail.com>
2025-11-13 09:31:53 +0300

    Update min_max_thread.cpp

--- lab2 c++20/src/min_max_thread.cpp
+++ lab2 c++20/src/min_max_thread.cpp
@@ -5,11 +5,13 @@ MinMaxResult g_minmax_result;
 std::mutex g_console_mutex;
 
 DWORD WINAPI find_min_max(LPVOID param) {
-    auto* arr = static_cast<std::vector<int>*>(param);
-    if (!arr || arr->empty()) return 1;
+    auto* arr = static_cast<std::vector<long long>*>(param);
+    if (!arr || arr->empty()) {
+        return 1;
+    }
 
-    int minVal = (*arr)[0];
-    int maxVal = (*arr)[0];
+    long long minVal = (*arr)[0];
+    long long maxVal = (*arr)[0];
 
     for (size_t i = 1; i < arr->size(); ++i) {
         minVal = (std::min)(minVal, (*arr)[i]);
@@ -20,7 +22,8 @@ DWORD WINAPI find_min_max(LPVOID param) {
     g_minmax_result = { minVal, maxVal };
 
     std::lock_guard lock(g_console_mutex);
-    std::cout << "[min_max] Min = " << minVal
-        << ", Max = " << maxVal << "\n";
+    std::cout << "Min = " << minVal << "\n";
+    std::cout << "Max = " << maxVal << "\n";
+
     return 0;
 }
