cee0a0bc5a6f99829457ae465c06cc088f52ae58
Maria <gufi7de@gmail.com>
2025-11-13 09:14:12 +0300

    add new tests

--- lab2 c++20/tests/test_average_thread.cpp
+++ lab2 c++20/tests/test_average_thread.cpp
@@ -6,3 +6,27 @@ TEST(AverageThread, CalculatesCorrectAverage) {
     find_average(&arr);
     EXPECT_DOUBLE_EQ(g_avg_result.value, 5.0);
 }
+
+TEST(AverageThread, AllEqualValues) {
+    std::vector<int> arr = { 3, 3, 3 };
+    find_average(&arr);
+    EXPECT_DOUBLE_EQ(g_avg_result.value, 3.0);
+}
+
+TEST(AverageThread, SingleElement) {
+    std::vector<int> arr = { 42 };
+    find_average(&arr);
+    EXPECT_DOUBLE_EQ(g_avg_result.value, 42.0);
+}
+
+TEST(AverageThread, HandlesNegativeNumbers) {
+    std::vector<int> arr = { -5, -15, -10 };
+    find_average(&arr);
+    EXPECT_DOUBLE_EQ(g_avg_result.value, -10.0);
+}
+
+TEST(AverageThread, MixedPositiveAndNegative) {
+    std::vector<int> arr = { -2, 4, -6, 8 };
+    find_average(&arr);
+    EXPECT_DOUBLE_EQ(g_avg_result.value, 1.0);
+}
