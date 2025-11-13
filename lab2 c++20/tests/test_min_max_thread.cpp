b3d377504638f7dcd6816da76bbae6f4d57cfa89
Maria <gufi7de@gmail.com>
2025-11-13 09:14:41 +0300

    add new tests

--- lab2 c++20/tests/test_min_max_thread.cpp
+++ lab2 c++20/tests/test_min_max_thread.cpp
@@ -7,3 +7,31 @@ TEST(MinMaxThread, FindsCorrectValues) {
     EXPECT_EQ(g_minmax_result.min, 1);
     EXPECT_EQ(g_minmax_result.max, 9);
 }
+
+TEST(MinMaxThread, AllElementsEqual) {
+    std::vector<int> arr = { 7, 7, 7, 7 };
+    find_min_max(&arr);
+    EXPECT_EQ(g_minmax_result.min, 7);
+    EXPECT_EQ(g_minmax_result.max, 7);
+}
+
+TEST(MinMaxThread, SingleElement) {
+    std::vector<int> arr = { 42 };
+    find_min_max(&arr);
+    EXPECT_EQ(g_minmax_result.min, 42);
+    EXPECT_EQ(g_minmax_result.max, 42);
+}
+
+TEST(MinMaxThread, HandlesNegativeNumbers) {
+    std::vector<int> arr = { -5, -1, -10, -3 };
+    find_min_max(&arr);
+    EXPECT_EQ(g_minmax_result.min, -10);
+    EXPECT_EQ(g_minmax_result.max, -1);
+}
+
+TEST(MinMaxThread, MixedPositiveAndNegative) {
+    std::vector<int> arr = { -8, 0, 15, -2, 9 };
+    find_min_max(&arr);
+    EXPECT_EQ(g_minmax_result.min, -8);
+    EXPECT_EQ(g_minmax_result.max, 15);
+}
