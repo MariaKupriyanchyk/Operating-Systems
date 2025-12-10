74ade95e835b43d7c8a7af658772766f5d10e7bf
Maria <gufi7de@gmail.com>
2025-12-10 18:58:10 +0300

    add more functions, update old functions

--- lab3/include/marker_manager.h
+++ lab3/include/marker_manager.h
@@ -1,46 +1,50 @@
 #pragma once
-#include "shared_array.h"
 #include <vector>
-#include <thread>
 #include <mutex>
-#include <condition_variable>
-#include <atomic>
-#include <memory>
+
+class SharedArray;
 
 class MarkerManager {
 public:
-    explicit MarkerManager(int size);
+    MarkerManager(int size);
     ~MarkerManager();
 
     void launch(int count);
     void start_all();
     void wait_all_blocked();
-    void terminate(int id);
+    bool terminate(int id);
     void resume_all();
     bool any_alive() const;
+    int alive_count() const;
+    int find_first_alive() const;
     void join_all();
     void print() const;
 
 private:
-    struct Marker {
-        std::thread th;
-        std::atomic<bool> waiting{ false };
-        std::atomic<bool> stop{ false };
-        std::atomic<bool> alive{ true };
-        std::mutex mtx;
-        std::condition_variable cv;
-    };
-
     void worker(int id);
     static void sleep_ms(int ms);
 
+    struct Marker {
+        std::thread th;
+        std::mutex mtx;
+        std::condition_variable cv;
+
+        std::atomic<bool> stop{ false };
+        std::atomic<bool> alive{ true };
+        std::atomic<bool> waiting{ false };
+
+        Marker() = default;
+        Marker(const Marker&) = delete;
+        Marker& operator=(const Marker&) = delete;
+    };
+private:
     std::shared_ptr<SharedArray> array_;
     std::vector<std::unique_ptr<Marker>> markers_;
-
-    std::mutex ctrl_mtx_;
+    mutable std::mutex ctrl_mtx_;
     std::condition_variable ctrl_cv_;
-
     std::atomic<bool> started_{ false };
     std::mutex start_mtx_;
     std::condition_variable start_cv_;
+
+    static std::mutex cout_mtx_;
 };
