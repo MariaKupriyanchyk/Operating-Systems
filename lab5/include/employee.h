95ad29ede74e26c56556688e084adbbbd7b598f3
Maria <gufi7de@gmail.com>
2026-01-02 18:38:01 +0300

    add functions for correct input data

--- lab5/include/employee.h
+++ lab5/include/employee.h
@@ -1,7 +1,18 @@
 #pragma once
+#include <string>
+#include <iostream>
 
 struct employee {
     int num;
     char name[10];
     double hours;
 };
+
+void input_id(int& id);
+void input_name(char name[10]);
+void input_hours(double& hours);
+void input_employee(employee& emp);
+void input_employee_update(employee& emp);
+
+bool read_int(int& value);
+bool read_double(double& value);
