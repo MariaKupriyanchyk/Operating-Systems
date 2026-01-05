c611ca61999ac68be21e8dc36a78d4196d1fb31c
Maria <gufi7de@gmail.com>
2026-01-05 22:41:14 +0300

    change work repository

--- colloquium api rest/TaskRepository.java
+++ colloquium api rest/TaskRepository.java
@@ -1,37 +1,5 @@
 package org.example;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
-
-@Repository
-public class TaskRepository {
-
-    private final Map<Long, Task> storage = new HashMap<>();
-    private final AtomicLong idGenerator = new AtomicLong(1);
-
-    public List<Task> findAll() {
-        return new ArrayList<>(storage.values());
-    }
-
-    public Optional<Task> findById(Long id) {
-        return Optional.ofNullable(storage.get(id));
-    }
-
-    public Task save(Task task) {
-        if (task.getId() == null) {
-            task.setId(idGenerator.getAndIncrement());
-        }
-        storage.put(task.getId(), task);
-        return task;
-    }
-
-    public void delete(Long id) {
-        storage.remove(id);
-    }
-
-    public boolean exists(Long id) {
-        return storage.containsKey(id);
-    }
-}
+public interface TaskRepository extends JpaRepository<Task, Long> {}
