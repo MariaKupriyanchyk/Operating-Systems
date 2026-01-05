10b2ab62923d536184eb11e7dd9a0a7a456a600d
Maria <gufi7de@gmail.com>
2026-01-05 22:43:19 +0300

    add caching

--- colloquium api rest/TodoApplication.java
+++ colloquium api rest/TodoApplication.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableCaching
 public class TodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class, args);
